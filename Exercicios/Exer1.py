01b0f271856292ab5293e2aa85014486bd428116
vincytarsis <vincydetarsis@gmail.com>
2021-12-27 17:05:43 -0300

    Exerc01

---
+++ Exercicios/Exer1.py
@@ -0,0 +1,3 @@
+#Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado')
+nome = input('Qual o seu nome? ' )
+print('Olá',nome,'! Prazer em te conhecer!')
