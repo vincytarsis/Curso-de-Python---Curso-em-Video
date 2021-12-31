9d0a2b4aa0ea716b2e8344224b89b1efd3b136f3
vincytarsis <vincydetarsis@gmail.com>
2021-12-31 07:48:57 -0300

    Update ex024.py

--- Curso em Video/Exercicios/ex024.py
+++ Curso em Video/Exercicios/ex024.py
@@ -1,8 +1,8 @@
 # Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com
 # o nome "SANTO".
 
-n = str(input('Digite o nome da sua cidade: '))
+n = str(input('Digite o nome da sua cidade TODA EM MAIÚSCULA : '))
 
-cs = n.split()
-
-print('Sua cidade começa com Santo?:{}'.format(n))
+cs = n.split()[0]
+ss = 'SANTO' in cs
+print('Sua cidade começa com Santo?:{}'.format(ss))
