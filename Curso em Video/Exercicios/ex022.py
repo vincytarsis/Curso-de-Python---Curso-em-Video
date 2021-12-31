a802f2a76990ccd5296aa77ebb6a60410f623bcc
vincytarsis <vincydetarsis@gmail.com>
2021-12-31 08:30:29 -0300

    Update ex022.py

--- Curso em Video/Exercicios/ex022.py
+++ Curso em Video/Exercicios/ex022.py
@@ -4,14 +4,16 @@
 # -Quantas letras ao todo (sem considerar espaços)
 # -Quantas letras tem o primeiro nome
 
-n = str(input('Digite o seu nome completo: '))
+n = str(input('Digite o seu nome completo: ')).strip()
 
 tmsc = n.upper() #Todoas as letras maiúsculas
 tmic = n.lower() #Todas as letras minúsculas
 qtlt = len(n.replace(' ',''))  #Quantas letras ao todo
+# len((n) - n.count(' ')) # Daria para colocar dessa forma também
 qlpn = len(n.split()[0]) #Quantas letras tem o primeiro nome
+# (n.find(' ')) # Também daria certo
 
 print('Nome com todas as letras maiúsculas:{}'.format(tmsc))
 print('Nome com todas as letras minúsculas:{}'.format(tmic))
-print('Quantas letras ao todo(sem considerar espaços:{}'.format(qtlt))
+print('Quantas letras ao todo(sem considerar espaços):{}'.format(qtlt))
 print('Quantas letras tem o primeiro nome:{}'.format(qlpn))
