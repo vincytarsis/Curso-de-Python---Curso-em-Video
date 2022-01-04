f81d234038604af8dd66d3b13212ab38af5691fd
vincytarsis <vincydetarsis@gmail.com>
2022-01-04 20:11:23 -0300

    Update ex045.py

--- Curso em Video/Exercicios/ex045.py
+++ Curso em Video/Exercicios/ex045.py
@@ -9,4 +9,16 @@ pc = random.randint(1, 3)
 if pc == jk:
     print('Empate')
 elif pc == 1 and jk == 2:
-    print('Você ganhou!\n')
+    print('Você ganhou!\n\Papel ganha de Pedra')
+elif pc == 1 and jk == 3:
+    print('Você perdeu\nPedra ganha de Tesoura')
+elif pc == 2 and jk == 1:
+    print('Você perdeu!\nPapel ganha de Pedra')
+elif pc == 2 and jk == 3:
+    print('Você ganhou!\nTesoura ganha de papel')
+elif pc == 3 and jk == 1:
+    print('Você ganhou\nPedra ganha de Tesoura')
+else:
+    print('Você perdeu!\nTesoura ganhde Papel')
+
+print(pc)
