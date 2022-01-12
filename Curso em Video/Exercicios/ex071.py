0ad8718523c9118b0c0502d1511493a806f2d777
vincytarsis <vincydetarsis@gmail.com>
2022-01-12 09:28:20 -0300

    Update ex071.py

--- Curso em Video/Exercicios/ex071.py
+++ Curso em Video/Exercicios/ex071.py
@@ -2,18 +2,30 @@
 qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor
 serão entregues. OBS: Considere que o caixa possui cédulas de R$50,R$20,R$10 e R$1."""
 
-
-
-notas50 = 0
-
 # Valor a ser sacado
 print('+'*15)
 print('Banco Tarsis')
 print('+'*15)
-while True:
-    valor = float(input('Qual o valor a ser sacado:'))
-    if valor >= 50:
-        notas50 = valor / 50
 
-# Quantidades de notas a ser sacadas
-print(f'Notas de 50 {notas50}')
+valor = int(input('Valor a ser sacado:R$'))
+total = valor
+ced = 50
+totced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} cédulas de R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        elif ced == 1:
+            ced = 0
+        totced = 0
+        if total == 0:
+            break
