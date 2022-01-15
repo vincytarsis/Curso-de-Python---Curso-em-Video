470006fd478a636cbbde62b49edd7e7bb81d525e
vincytarsis <vincydetarsis@gmail.com>
2022-01-15 07:43:30 -0300

    Update aula017.py

--- Curso em Video/Aulas/aula017.py
+++ Curso em Video/Aulas/aula017.py
@@ -16,9 +16,9 @@ valores.append(5)
 valores.append(9)
 valores.append(4)
 
-#for v in enumerate(valores):#enumera
+for v in enumerate(valores):#enumera
 #for v in valores:
-    #print(f'{v}...',end='')
+    print(f'{v}...', end='')
 
 #for cont in range(0, 5):
     #valores.append(int(input('Digite um valor')))
@@ -29,8 +29,8 @@ valores.append(4)
 #print('Cheguei ao fina da lista')
 
 
-a = [2,3,4,7]
-b=a[:]#recebe uma copia dos valores de b
-b[2] = 8 # liga uma lista com outra
-print(f'Lista A: {a}')
-print(f'Lista B :{b}')
+#a = [2,3,4,7]
+#b=a[:]#recebe uma copia dos valores de b
+#b[2] = 8 # liga uma lista com outra
+#print(f'Lista A: {a}')
+#print(f'Lista B :{b}')
