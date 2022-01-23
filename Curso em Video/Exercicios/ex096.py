3cf58e70103727e2e3a8797e6368f0c1935ac9ca
vincytarsis <vincydetarsis@gmail.com>
2022-01-23 08:28:56 -0300

    Update ex096.py

--- Curso em Video/Exercicios/ex096.py
+++ Curso em Video/Exercicios/ex096.py
@@ -2,10 +2,23 @@
 retangular(largura e comprimento) e mostre a área do terreno. """
 
 
-def area(a, b):
+"""def area(a, b):
     terreno = a * b
     print(f'A area de um terreno {a}x{b} é de {terreno}m²')
 
 
-area(a=float(input('A:')), b=float(input('B:')))
+area(a=float(input('A:')), b=float(input('B:')))"""
 
+# Guanabara
+
+
+def area(larg, comp):
+    a = larg * comp
+    print(f'A área de um terreno {larg}x{comp} é de {a}m².')
+
+
+print('Controle de Terrenos')
+print('-' * 20)
+l = float(input('Largura (m): '))
+c = float(input('Comprimento (m): '))
+area(l, c)
