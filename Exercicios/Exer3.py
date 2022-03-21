9c54e9a2db6028c2b5395a9a1534708a1804682e
Vincy de Tarsis <96235421+vincytarsis@users.noreply.github.com>
2022-03-21 15:08:02 -0300

    Delete Exercicios directory

--- Exercicios/Exer3.py
+++
@@ -1,6 +0,0 @@
-#Crie um script Python que leia dois números e tente mostra a soma entre eles.
-
-n1 = input('Primeiro número: ')
-n2 = input('Segundo número: ')
-soma = int(n1)+int(n2)
-print('A sona é ', str(soma))
