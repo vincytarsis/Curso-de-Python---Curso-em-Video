7ce58c3fce3dfd0af3718ef8b6a0a87cc25231ba
VincydeTarsis <vincydetarsis@gmail.com>
2022-03-25 04:39:58 -0300

    Modorização

---
+++ Curso em Video/Exercicios/ex107.py
@@ -0,0 +1,5 @@
+""" Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro(),
+ e metade(). Faça também um programa que importe esse módulo e use algumas dessas funções."""
+
+p = float(input('Digite o preço: R$'))
+print(f'A metade de {p}')
